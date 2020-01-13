42049944e44f5a47ae23365170de4c1472e02ba3
Aline Bezzoco <alinebezzoco@MacBook-Air-de-Aline.local>
2020-01-13 02:26:49 -0300

    Updates

---
+++ precache-manifest.fc8c9b99df803e782ff3d5c47632e231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3085dc4f916c64daefc4899ef948dc93",
+    "url": "/ibge-search-name/index.html"
+  },
+  {
+    "revision": "466241035b450ac52b14",
+    "url": "/ibge-search-name/static/css/main.b5d54447.chunk.css"
+  },
+  {
+    "revision": "310d6c6eb381c87c2ab5",
+    "url": "/ibge-search-name/static/js/2.7b94ace7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ibge-search-name/static/js/2.7b94ace7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "466241035b450ac52b14",
+    "url": "/ibge-search-name/static/js/main.a14a3eb1.chunk.js"
+  },
+  {
+    "revision": "8a8db9d24e669ba24a0c",
+    "url": "/ibge-search-name/static/js/runtime-main.b90b7217.js"
+  }
+]);
